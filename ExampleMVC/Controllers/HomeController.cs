5a802ce63a37cf7eb0f9a7cef0b16f4bec87d89c
PSPDFKit <support@pspdfkit.com>
2021-03-12 13:20:15 +0530

    PSPDFKit for Web ASP.NET Example

---
+++ ExampleMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult StandaloneExample()
+        {
+            ViewBag.Message = "StandaloneExample";
+
+            return View();
+        }
+
+        public ActionResult ServerExample()
+        {
+            ViewBag.Message = "ServerExample";
+
+            return View();
+        }
+    }
+}
