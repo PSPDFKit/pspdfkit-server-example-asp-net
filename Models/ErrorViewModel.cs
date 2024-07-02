1efdd34219b12a0180c1c3bfda0fa1885529806f
PSPDFKit <support@pspdfkit.com>
2024-07-02 09:01:21 +0200

    PSPDFKit for Web ASP.NET Example

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace pspdfkit_aspnet_example.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
